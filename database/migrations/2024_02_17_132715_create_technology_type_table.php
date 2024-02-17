5a32471f0dc1f37987e2f631186c04c294c520e3
Michelazani <michi.zani220@gmail.com>
2024-02-17 17:06:45 +0100

    creazione tabella ponte

---
+++ database/migrations/2024_02_17_132715_create_technology_type_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('technology_type', function (Blueprint $table) {
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('types')->cascadeOnDelete();
+
+            $table -> unsignedBigInteger('technology_id');
+            $table->foreign('technology_id')->references('id')->on('technologies');
+            
+            $table->primary(['type_id', 'technology_id']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('technology_type');
+    }
+};
